3fbd9a0d6010dda5d0bd1238e75e9ec1ea2d4885
anpun <yugi_an@hotmail.com>
2021-02-09 16:24:38 +0700

    - edit  LatestNewsTabView

--- lib/tabview/latestNews_tab_view.dart
+++ lib/tabview/latestNews_tab_view.dart
@@ -1,13 +1,14 @@
-//import 'dart:html';
-
-//import 'package:New_Project_KMUTTNEWS/tabview/read_news_view.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_core/firebase_core.dart';
+
+//import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 class LatestNewsTabView extends StatefulWidget {
-  LatestNewsTabView({Key key, this.title}) : super(key: key);
-  final String title;
+  LatestNewsTabView({Key key, this.newsTitle, this.newsContent})
+      : super(key: key);
+  final String newsTitle;
+  final String newsContent;
+
   @override
   _LatestNewsTabViewState createState() => _LatestNewsTabViewState();
 }
@@ -16,95 +17,58 @@ class _LatestNewsTabViewState extends State<LatestNewsTabView> {
   @override
   void initState() {
     super.initState();
-    // Firebase.initializeApp().whenComplete(() {
-    //   print("object");
-    //   setState(() {});
-    // });
+    Firestore.instance
+        .collection("News")
+        .document(widget.newsTitle)
+        .get()
+        .then((value) {
+      setState(() {});
+    });
+    //Firebase.initializeApp();
   }
 
   @override
   Widget build(BuildContext context) {
-    // CollectionReference news = FirebaseFirestore.instance.collection("")
     return Scaffold(
-//         body: StreamBuilder(
-//             stream: Firestore.instance.collection("News").snapshots(),
-//             builder: (context, snapshot) {
-//               if (!snapshot.hasData) {
-//                 return Center(
-//                   child: Column(
-//                     children: <Widget>[
-//                       CircularProgressIndicator(),
-//                       Text("Loading..........."),
-//                     ],
-//                   ),
-//                 );
-//               } else {
-//                 return ListView.builder(
-//                   itemCount: snapshot.data.documents.length,
-//                   itemBuilder: (context, index) {
-//                     return Padding(
-//                       padding: const EdgeInsets.all(8.0),
-//                       child: Card(
-//                         child: Container(
-//                           child: InkWell(
-//                             onTap: () {
-//                               Navigator.push(
-//                                   context,
-//                                   MaterialPageRoute(
-//                                       builder: (context) => ReadNewsView(
-//                                             newsTitle: snapshot.data
-//                                                 .documents[index].documentID,
-//                                             newsContent: snapshot.data
-//                                                 .documents[index].data["title"],
-//                                           )));
-//                             },
-//                             child: Column(
-//                               children: <Widget>[
-//                                 ListTile(
-//                                   title: Text(snapshot
-//                                       .data.documents[index].documentID),
-//                                   subtitle: Text(snapshot
-//                                       .data.documents[index].data["title"]),
-//                                 ),
-//                               ],
-//                             ),
-//                           ),
-//                         ),
-//                       ),
-//                     );
-//                   },
-//                 );
-//               }
-//             }));
-//   }
-// }
-      body: FutureBuilder(
-        future: getData(),
-        builder: (context, AsyncSnapshot<DocumentSnapshot> snapshot) {
-          if (snapshot.connectionState == ConnectionState.done) {
-            return Column(
-              children: [
-                Container(
-                  height: 27,
-                  child: Text(
-                    "Name : ${snapshot.data.data()['title']}",
-                    overflow: TextOverflow.fade,
-                    style: TextStyle(fontSize: 20),
-                  ),
+      body: StreamBuilder(
+          stream: Firestore.instance.collection("News").snapshots(),
+          builder: (context, snapshot) {
+            if (!snapshot.hasData) {
+              return Center(
+                child: Column(
+                  children: <Widget>[
+                    CircularProgressIndicator(),
+                    Text("Loading........."),
+                  ],
                 ),
-              ],
-            );
-          } else if (snapshot.connectionState == ConnectionState.none) {
-            return Text("No Data");
-          }
-          return CircularProgressIndicator();
-        },
-      ),
+              );
+            } else {
+              return ListView.builder(
+                  itemCount: snapshot.data.documents.length,
+                  itemBuilder: (context, index) {
+                    return Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Card(
+                        child: Container(
+                          child: InkWell(
+                            onTap: () {},
+                            child: Column(
+                              children: [
+                                ListTile(
+                                  title: Text(snapshot
+                                      .data.documents[index].documentID),
+                                  subtitle: Text(
+                                      snapshot.data.documents[index]["title"]),
+                                )
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                    );
+                  });
+            }
+          }),
     );
-  }
-
-  Future<DocumentSnapshot> getData() async {
-    await Firebase.initializeApp();
-    return await FirebaseFirestore.instance.collection("News").doc("IT").get();
   }
 }
